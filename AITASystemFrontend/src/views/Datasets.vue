064a0423bb0cb8b2cfd02518641e8f12f6e18131
chl <2334597745@qq.com>
2024-05-08 13:53:17 +0800

    datasets

--- AITASystemFrontend/src/views/Datasets.vue
+++ AITASystemFrontend/src/views/Datasets.vue
@@ -1,12 +1,55 @@
 <template>
-  <div class="datasets">
-    {{ route.query.type }}
-  </div>
+  <div class="data">{{ "数据集" }}</div>
+  <ul v-if="dataList.length > 0">
+     <li v-for="data in dataList" :key="data.data_id">
+         <h3>{{ data.data_title }}</h3>
+         <p>{{ data.data_content }}</p>
+         <!-- 添加更多需要展示的属性... -->
+      </li>
+    </ul>
+    <p v-else class="no-data-message">暂无数据集数据</p>
 </template>
 
 <script setup lang="ts" name="Datasets">
 import { ref, reactive, toRefs, onBeforeMount, onMounted, watchEffect, computed } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
+import axios from 'axios';
+interface data {
+       data_id: number;
+       data_title: string;
+       data_content: string;
+       data_date: string;
+       data_author: string;
+       data_link: string;
+       data_read_count: number;
+       data_image_url: string;
+       label_id: number;
+     }
+     let dataList = ref<data[]>([]);
+     
+const route1 = useRoute();
+const typeParam = route1.query.type as string; // 使用 as string 可以指定参数类型为字符串，避免类型推断问题
+// 然后可以在组件中使用 typeParam，比如在模板中显示
+console.log('Type 参数值:', typeParam);
+
+onMounted(() => {
+       // 发起 Axios 请求获取新闻数据
+       axios.get('http://127.0.0.1:5000/datasets', {
+  params: {
+    type: typeParam
+  }
+})
+         .then((response) => {
+           if (response.data) {
+             dataList.value = response.data;  // 将获取到的数据赋值给 dataList
+           } else {
+             console.error('Failed to fetch data data.');
+           }
+         })
+         .catch((error) => {
+           console.error('An error occurred while fetching data:', error);
+         });
+     });
 /**
 * 路由对象
 */
